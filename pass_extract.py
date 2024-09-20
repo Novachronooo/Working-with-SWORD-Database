c45474035f108249a49e174dbad74e24794b2a05
Suraj Khatri <khatrisj@mail.uc.edu>
2024-09-19 22:16:43 -0400

    A python script that stores the shapefile into a geo dataframe, and useing the data frame extracts value from 'swot_orbit' column and splits them by spaces and stores into a pass_number set, which will act as our pass number for the main program

---
+++ pass_extract.py
@@ -0,0 +1,37 @@
+import glob
+import h5netcdf
+import xarray as xr
+import pandas as pd
+import geopandas as gpd
+import contextily as cx
+import numpy as np
+import matplotlib.pyplot as plt
+import hvplot.xarray
+import zipfile
+import earthaccess
+
+# Authentication for Earth Access
+auth = earthaccess.login(persist=True)
+
+input_shapefile = "D:\\Environment Engineering\\Project\\ARCGIS\\OHIO_Basin_Extracted_From_HB74.shp"
+
+#read shapefile using geopandas
+Ohio_Basin_df = gpd.read_file(input_shapefile)
+
+pass_numbers = set()
+
+column_name = 'swot_orbit'
+#loop through each row in the shapefile
+for index, row in Ohio_Basin_df.iterrows():
+    #split the pass numbers in 'swot_orbit' column column 
+    if pd.notna(row[column_name]) and row[column_name] is not None:
+        orbit_values = [values.strip() for values in row[column_name].split()]
+
+        #add the pass numbers to the set
+        pass_numbers.update(orbit_values)
+
+#convert the set to a list
+pass_numbers = list(pass_numbers)  
+print(pass_numbers)
+
+
